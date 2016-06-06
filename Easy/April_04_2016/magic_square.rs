180c174ca2cbb4f28b8cb63c71940893bf75fe75
Vishal Sodani <vishalsodani@rediffmail.com>
2016-06-06 13:06:31 +0530

    format code

--- Easy/April_04_2016/magic_square.rs
+++ Easy/April_04_2016/magic_square.rs
@@ -5,34 +5,34 @@ fn test_magic_square()-> bool {
     let mut sum = 0;
 
     for number in magic_square.iter() {
-            if start >= check_length {
-                if sum != 15 {
-                    return false;
-                }
-                start = 0 ;
-                sum = 0;
+        if start >= check_length {
+            if sum != 15 {
+                return false;
             }
-            sum = sum + *number ;
-            start = start + 1;
+            start = 0 ;
+            sum = 0;
+        }
+        sum = sum + *number ;
+        start = start + 1;
     }
-	//columns
-	for x in 0..check_length {
-		sum = magic_square[x] + magic_square[x+3] + magic_square[x+3+3];
-		if sum != 15 {
-			return false;
-		}
-	}
-	//diagonals
-	if magic_square[0] + magic_square[4] + magic_square[8] != 15 {
-		return false;
-	}
-	if magic_square[2] + magic_square[4] + magic_square[6] != 15 {
-		return false;
-	}
-		true
-	}
+    //columns
+    for x in 0..check_length {
+        sum = magic_square[x] + magic_square[x+3] + magic_square[x+3+3];
+        if sum != 15 {
+            return false;
+        }
+    }
+    //diagonals
+    if magic_square[0] + magic_square[4] + magic_square[8] != 15 {
+        return false;
+    }
+    if magic_square[2] + magic_square[4] + magic_square[6] != 15 {
+        return false;
+    }
+        true
+    }
 
-	fn main(){
-		let result = test_magic_square() ;
-		println!("{:?}", result );
-	}
+fn main(){
+    let result = test_magic_square() ;
+    println!("{:?}", result );
+}
