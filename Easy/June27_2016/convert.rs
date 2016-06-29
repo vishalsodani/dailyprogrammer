cdfa5f60e840646e356346ef27e6fa04e90f2ecb
Vishal Sodani <vishalsodani@rediffmail.com>
2016-06-29 19:25:15 +0530

    refactored to use &str as parameter for convert as no string modifications needed
    converted from if to using match and using Option type to get output

--- Easy/June27_2016/convert.rs
+++ Easy/June27_2016/convert.rs
@@ -1,23 +1,24 @@
-fn convert(input:String){
+fn convert(input:&str){
     let length = input.len() ;
-    let number = &input[0..length-2].parse::<f32>().unwrap() ;
+    let number = input[0..length-2].parse::<f32>().unwrap() ;
+	let conversion_direction = &input[length-2..length] ;
 
-    if input.ends_with("rd") {
-        let x =  number * ( 180.0 / 3.1416) ;
-        println!("{0}{1}", x, "d" );
-    }
-    else if input.ends_with("dr") {
-        let x = number * ( 3.1416 / 180.0 ) ;
-        println!("{0}{1}", x, "r" );
-    }
-    else {
-        println!("No candidate for conversion" );
-    }
+	let result:Option<f32> = match conversion_direction {
+		"rd" => Some(number * ( 180.0 / 3.1416)) ,
+		"dr" => Some(number * ( 3.1416 / 180.0 )) ,
+		_ => None ,
+	} ;
 
+	if result == None {
+		println!("No candidate for conversion" );
+	}
+	else {
+		println!("{0}{1}", result.unwrap() , conversion_direction[1..2].to_string() );
+	}
 }
 
 fn main() {
-    convert("3.1416rd".to_string()) ;
-    convert("90dr".to_string()) ;
-    convert("100cr".to_string()) ;
+    convert("3.1416rd") ;
+    convert("90dr") ;
+    convert("100cr") ;
 }
