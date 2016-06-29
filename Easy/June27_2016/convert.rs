280be546b10d8e660e0b56b2aa80b22eeabf89c6
Vishal Sodani <vishalsodani@rediffmail.com>
2016-06-29 15:43:39 +0530

    handle no conversion

--- Easy/June27_2016/convert.rs
+++ Easy/June27_2016/convert.rs
@@ -11,11 +11,14 @@ fn convert(mut input:String){
         let x = number * ( 3.1416 / 180.0 ) ;
         println!("{0}{1}", x, to );
     }
+    else {
+        println!("No candidate for conversion" );
+    }
 
 }
-
 
 fn main() {
     convert("3.1416rd".to_string()) ;
     convert("90dr".to_string()) ;
+    convert("100cr".to_string()) ;
 }
