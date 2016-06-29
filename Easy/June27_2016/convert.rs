fe25381a3b0cb7a20aef2003b3b645be2458189d
Vishal Sodani <vishalsodani@rediffmail.com>
2016-06-29 17:40:11 +0530

    replace doing mutation of the input string with using endswith and slice
    to get conversion type and input number

--- Easy/June27_2016/convert.rs
+++ Easy/June27_2016/convert.rs
@@ -1,15 +1,14 @@
-fn convert(mut input:String){
-    let to = input.pop().unwrap() ;
-    let from = input.pop().unwrap() ;
-    let number =input.parse::<f32>().unwrap() ;
+fn convert(input:String){
+    let length = input.len() ;
+    let number = &input[0..length-2].parse::<f32>().unwrap() ;
 
-    if from == 'r' && to == 'd' {
+    if input.ends_with("rd") {
         let x =  number * ( 180.0 / 3.1416) ;
-        println!("{0}{1}", x, to );
+        println!("{0}{1}", x, "d" );
     }
-    else if from == 'd' && to == 'r' {
+    else if input.ends_with("dr") {
         let x = number * ( 3.1416 / 180.0 ) ;
-        println!("{0}{1}", x, to );
+        println!("{0}{1}", x, "r" );
     }
     else {
         println!("No candidate for conversion" );
