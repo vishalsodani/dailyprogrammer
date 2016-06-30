4a68b7255333eb595258b507b19928def047a96f
Vishal Sodani <vishalsodani@rediffmail.com>
2016-06-30 08:46:23 +0530

    handle the bonus inputs

--- Easy/June27_2016/convert.rs
+++ Easy/June27_2016/convert.rs
@@ -6,6 +6,9 @@ fn convert(input:&str){
 	let result:Option<f32> = match conversion_direction {
 		"rd" => Some(number * ( 180.0 / 3.1416)) ,
 		"dr" => Some(number * ( 3.1416 / 180.0 )) ,
+		"fc" => Some(( number - 32.0 ) * ( 5.0 / 9.0 )) ,
+		"cf" => Some(( number * ( 9.0 / 5.0 )) + 32.0 ) ,
+		"kc" => Some( number - 273.15 ) ,
 		_ => None ,
 	} ;
 
@@ -13,12 +16,15 @@ fn convert(input:&str){
 		println!("No candidate for conversion" );
 	}
 	else {
-		println!("{0}{1}", result.unwrap() , conversion_direction[1..2].to_string() );
+		println!("{0}{1}", result.unwrap(), conversion_direction[1..2].to_string() );
 	}
 }
 
 fn main() {
     convert("3.1416rd") ;
     convert("90dr") ;
+	convert("212fc") ;
+	convert("70cf") ;
+	convert("315.15kc") ;
     convert("100cr") ;
 }
