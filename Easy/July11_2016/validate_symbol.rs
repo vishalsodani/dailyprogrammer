be19d9b8f63ab8d0514e218844843ab54ba18a0b
Vishal Sodani <vishalsodani@rediffmail.com>
2016-07-12 13:22:17 +0530

    small refactoring

--- Easy/July11_2016/validate_symbol.rs
+++ Easy/July11_2016/validate_symbol.rs
@@ -1,28 +1,28 @@
 fn valid(element:&str, symbol:&str) -> bool {
-	
+
 	let mut found_first_char = false;
 	let mut found_second_char = false;
-	let first_char = symbol.chars().nth(0).unwrap().to_string();
-	let second_char = symbol.chars().nth(1).unwrap().to_string();
-	
+	let first_char = symbol.chars().nth(0).unwrap().to_string().to_lowercase();
+	let second_char = symbol.chars().nth(1).unwrap().to_string().to_lowercase();
+
 	for character in element.chars() {
 		if found_first_char && found_second_char == false {
-			if character.to_string().to_lowercase() == second_char.to_lowercase() {
+			if character.to_string().to_lowercase() == second_char {
 				found_second_char = true;
 			}
 		}
-		
+
 		if found_first_char == false {
-			if character.to_string().to_lowercase() == first_char.to_lowercase() {
+			if character.to_string().to_lowercase() == first_char {
 				found_first_char = true;
 			}
 		}
 	}
-	
+
 	if found_first_char && found_second_char {
 		return true;
 	}
-	
+
 	return false;
 }
 
@@ -35,4 +35,3 @@ fn main() {
 	println!("{}", valid("Tullium", "Ty"));
 
 }
-	
